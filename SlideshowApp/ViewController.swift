03e7349fcc6763ecc77bba3effb58ae9fdefa6bf
kenichi04 <slow_life_1407@yahoo.co.jp>
2020-12-26 17:45:24 +0900

    スライドショー機能追加

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,12 +9,85 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var backBtn: UIButton!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    // 表示画像の番号
+    var numberOfPhoto = 0
+    let imageArray = ["meal1", "meal2", "meal3"]
+    
+    var timer: Timer!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func nextButton(_ sender: UIButton) {
+        numberOfPhoto += 1
+        if numberOfPhoto >= imageArray.count {
+            numberOfPhoto = 0
+        }
+        let photoName = imageArray[numberOfPhoto]
+        
+        let image = UIImage(named: photoName)
+        imageView.image = image
+        
+    }
+    
+    @IBAction func backButton(_ sender: UIButton) {
+        numberOfPhoto -= 1
+        if numberOfPhoto < 0 {
+            numberOfPhoto = imageArray.count - 1
+        }
+        let photoName = imageArray[numberOfPhoto]
+        
+        let image = UIImage(named: photoName)
+        imageView.image = image
+    }
+    
+    @IBAction func playButton(_ sender: UIButton) {
+        
+        if timer != nil {
+            nextBtn.isEnabled = true
+            backBtn.isEnabled = true
+            playBtn.setTitle("再生", for: .normal)
+            
+            // タイマー・スライド停止処理
+            if timer != nil {
+                timer.invalidate()
+                timer = nil
+            }
+            
+        } else {
+            nextBtn.isEnabled = false
+            //nextBtn.isHidden = true
+            backBtn.isEnabled = false
+            playBtn.setTitle("停止", for: .normal)
+            
+            // タイマー・スライド開始処理
+            if timer == nil {
+                timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(slideshow(_:)), userInfo: nil, repeats: true)
+            }
+        }
+        
+    }
+    
+    @objc func slideshow(_ timer: Timer) {
+        numberOfPhoto += 1
+        if numberOfPhoto >= imageArray.count {
+            numberOfPhoto = 0
+        }
+        let photoName = imageArray[numberOfPhoto]
+        
+        let image = UIImage(named: photoName)
+        imageView.image = image
+    }
+    
+    
+    
 }
 
